19979e8f50ec17a0818f238459f3a3c1f05f2c51
ChristmasLover12345 <madnes0205@gmail.com>
2025-04-01 12:14:27 -0700

    Dodecahedron

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement )
+
+const geometry = new THREE.DodecahedronGeometry(8);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00});
+const cube = new THREE.Mesh( geometry, material);
+scene.add(cube)
+
+camera.position.z = 25
+
+function animate() {
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.002;
+    renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate)
